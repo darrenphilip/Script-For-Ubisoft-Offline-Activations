22c93b0513542d95511f4b15ca2f8e428dfb32b6
renn <85210688+darrenphilip@users.noreply.github.com>
2024-07-18 03:40:28 +0700

    Changed script

--- script.ps1
+++ script.ps1
@@ -1,9 +1,11 @@
+# Define the URL of the executable file
 $url = "https://raw.githubusercontent.com/darrenphilip/Script-For-Ubisoft-Offline-Activations/main/script.ps1"
+
+# Define the local output path for the downloaded executable
 $output = "offlinescript.exe"
 
-# Download the executable
+# Download the executable file
 Invoke-WebRequest -Uri $url -OutFile $output
 
-# Run the executable
+# Run the downloaded executable
 Start-Process -FilePath $output -NoNewWindow
-
