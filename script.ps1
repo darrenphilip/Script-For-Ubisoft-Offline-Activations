95a481ef815c5879650788067424bf282977fbd6
renn <85210688+darrenphilip@users.noreply.github.com>
2024-07-18 03:33:41 +0700

    Fixed error

--- script.ps1
+++ script.ps1
@@ -1,4 +1,4 @@
-$url = "https://github.com/darrenphilip/Script-For-Ubisoft-Offline-Activations/blob/main/offlinescript/bin/Release/net8.0/win-x64/offlinescript.exe"
+$url = "https://raw.githubusercontent.com/darrenphilip/Script-For-Ubisoft-Offline-Activations/main/script.ps1"
 $output = "offlinescript.exe"
 
 # Download the executable
@@ -6,3 +6,4 @@ Invoke-WebRequest -Uri $url -OutFile $output
 
 # Run the executable
 Start-Process -FilePath $output -NoNewWindow
+
